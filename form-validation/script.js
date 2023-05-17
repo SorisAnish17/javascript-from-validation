31c207761908b534c32c4c457ea71613aa88bad7
SorisAnish17 <125728458+SorisAnish17@users.noreply.github.com>
2023-05-17 19:45:06 +0530

    Add files via upload

---
+++ form-validation/script.js
@@ -0,0 +1,81 @@
+let form = document.querySelector("#form");
+let firstName = document.forms[0][0];
+let lastName = document.forms[0][1];
+let address = document.forms[0][2];
+let email = document.forms[0][3];
+let plan = document.forms[0][6];
+let terms = document.forms[0][7];
+let radio = document.querySelector(".radio");
+let submit = document.querySelector("#submit");
+let formArea = document.querySelector(".form-area");
+//submit action
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  for (let childrens of form) {
+    console.log(childrens);
+    if (childrens.value) {
+      let smallEle = childrens.nextElementSibling;
+      if (smallEle?.tagName === "SMALL") {
+        smallEle.className = "success";
+        smallEle.textContent = childrens.value;
+      }
+    } else {
+      let smallEle = childrens.nextElementSibling;
+      if (smallEle?.tagName === "SMALL") {
+        smallEle.className = "error";
+        smallEle.textContent = "error";
+      }
+    }
+    //gender
+    let genders = document.querySelectorAll("[type='radio']");
+    let para = document.querySelector("#para");
+    let selectedOption;
+    for (let selectedGenders of genders) {
+      if (selectedGenders.checked) {
+        selectedOption = selectedGenders.value;
+      }
+    }
+    if (selectedOption == null) {
+      para.textContent = "select something";
+      para.style.color = "red";
+    } else {
+      para.textContent = selectedOption;
+      para.style.color = "green";
+    }
+  }
+  let paraTwo = document.querySelector("#paraTwo");
+  let planDetail = plan.options;
+  let detailsValue;
+  for (let details of planDetail) {
+    if (details.selected) {
+      detailsValue = details.value;
+    }
+  }
+  if (detailsValue) {
+    paraTwo.textContent = detailsValue;
+    paraTwo.style.color = "green";
+  } else {
+    paraTwo.textContent = "Error";
+    paraTwo.style.color = "red";
+  }
+});
+terms.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    formArea.style.backgroundColor = "lightYellow";
+  } else {
+    formArea.style.backgroundColor = "";
+  }
+});
+let inputText = document.querySelectorAll("[type='text']");
+let textArea = document.querySelector("#textArea");
+for (let data of inputText)
+  data.addEventListener("focus", (e) => {
+    data.style.borderColor = "lightGreen";
+  });
+textArea.addEventListener("focus", (e) => {
+  textArea.style.borderColor = "lightGreen";
+});
+let emailInput = document.querySelector("#email");
+emailInput.addEventListener("focus", (e) => {
+  emailInput.style.borderColor = "lightGreen";
+});
